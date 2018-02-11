ce09d5a7bf51c587d6387863eeb247ac4e25531b
Bhargav Srinivasan <bhargav461995@gmail.com>
2018-02-10 18:36:37 -0800

    Add CSS styling

--- src/App.js
+++ src/App.js
@@ -77,31 +77,36 @@ class App extends Component {
   render() {
     const {searchTerm, list}=this.state;
     return (
-      <div className="App">
+      <div className="page">
+        <div className="interactions">
         <Search
             value={searchTerm}
             onChange={this.onSearchChange}
         >
           Search
         </Search>
+        </div>
 
+        <div className="table">
         {
           list.filter(isSearched(this.state.searchTerm)).map(item =>
-            <div key={ item.objectID }>
-              <span>
+            <div key={ item.objectID } className="table-row">
+              <span style={{ width: '40%' }}>
                 <a href={item.url}>{ item.title }</a>
               </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-                <span>
-                    <Button onClick={() => this.onDismiss(item.objectID)}>
-                            Dismiss
-                    </Button>
-                </span>
+              <span style={{ width: '30%' }}>{item.author}</span>
+              <span style={{ width: '10%' }}>{item.num_comments}</span>
+              <span style={{ width: '10%' }}>{item.points}</span>
+              <span style={{ width: '10%' }}>
+                  <Button onClick={() => this.onDismiss(item.objectID)}
+                    className="button">
+                          Dismiss
+                  </Button>
+              </span>
             </div>
           )
         }
+        </div>
       </div>
     );
   }
